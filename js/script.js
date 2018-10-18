4d2c6be920e40c91be5b73d5e9c99726302feb7d
Анастасия Нестерюкова <nestery-nastya@mail.ru>
2018-10-18 23:05:34 +0600

    Add slider

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let sliderImages = {
+    'dot1': "url('./img/francesco-casalino-531157-unsplash.jpg')",
+    'dot2': "url('./img/main-img.jpg')",
+    'dot3': "url('./img/photo-1517991104123-1d56a6e81ed9.jpg')"
+};
+
+let prevSlider = '#dot2';
+
+$(document).ready(function(){     
+    $(".dot").click(function(event){        
+        $(prevSlider).removeClass("active");
+        $(".main").css("background-image", sliderImages[event.target.id]);
+        $(event.target).addClass("active");
+        prevSlider = '#' + event.target.id;
+    });
+});
